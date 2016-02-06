0f19ebabf0917b7cf557753982d47067b6118212
sqiggg <github@sqiggg.com>
2016-02-06 01:15:39 +0000

    Removed backups

--- BACKUP/test.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-Height = 200
-img = Image.open('images/img0.jpg')
-wpercent = (Height / float(img.size[0]))
-Width = int((float(img.size[1]) * float(wpercent)))
-
-img = img.resize((Width, Height), Image.ANTIALIAS)
-img.save('imgResized.jpg')
